17815d5e1c59645a140516db4a64df157e7ca2ae
jiangxiaoxin <jiangyanxin1989@gmail.com>
2023-02-15 10:38:19 +0800

    feat:配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from "path"
+
+console.log("=============", __dirname, path.resolve(__dirname, "./src"))
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 })
